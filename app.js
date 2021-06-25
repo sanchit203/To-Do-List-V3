4f32d3afbdea435643bfd6b6efd7fcad17a17aa1
sanchit203 <sanchitmunjal203@gmail.com>
2021-06-26 00:05:30 +0530

    Google auth added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const  mongoose = require('mongoose')
-const passport = require('passport')
-const passportLocalMongoose = require('passport-local-mongoose')
-const session = require("express-session")
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const  mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const session = require("express-session");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 const port = 3000;
@@ -24,17 +27,37 @@ mongoose.connect("mongodb://localhost:27017/loginDB",{useNewUrlParser: true, use
 
 const userSchema = new mongoose.Schema({
     username : String,
-    password : String
+    password : String,
+    googleId : String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  })
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', (req, res) => {
     if(req.isAuthenticated()){
@@ -52,6 +75,15 @@ app.get("/signup", function(req, res){
 app.get("/login", function(req,res){
     res.render("login");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 app.post("/signup", function(req,res){
     User.register({username : req.body.username}, req.body.password, function(err, user){
